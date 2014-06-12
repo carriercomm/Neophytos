5f316cc15940c58cfb0bdc2af464a1826310493c
kmcguire <kmcg3413@gmail.com>
2014-06-11 21:56:17 -0500

    changed server to use cert/prikey file and did house cleaning in pubcrypt @ did a lot of house cleaning in pubcrypt with cleaning up names   and splitting functionality out and naming it appropriately + added ability to read cert file with included private key @ server now uses cert file to get public and private keys instead   of needing a separate file for the public and private key; so now   the same way you generate a certification and key for SSL will also   work for the XORMIX

--- pubcrypt.py
+++ pubcrypt.py
@@ -1,6 +1,7 @@
 import math
 import base64
 import struct
+import pprint
 
 '''
 	This function expects bytes not str.
@@ -57,6 +58,68 @@ def decrypt(data, key):
 	data = toi256(data)
 	data = pow(data, prikey, pubkey)
 	return fromi256(data)
+
+'''
+	The cert file contains the certificate and the private key. We only
+	want to read our the private key lines in BASE64 and return them.
+'''
+def readKeyFromCertFile(path):
+	fd = open(path, 'r')
+	lines = fd.readlines()
+	fd.close()
+	
+	code = []
+	inside = False
+	
+	for line in lines:
+		line = line.strip()
+		if line.find('--BEGIN PRIVATE KEY--') > -1:
+			inside = True
+			continue
+		if line.find('--END PRIVATE KEY--') > -1:
+			code = ''.join(code)
+			return base64.b64decode(bytes(code, 'utf8'))
+		if inside:
+			code.append(line)
+			
+	# get someone's attention.. do not just return None
+	raise Exception('The provided key file [%s] had not private key.' % path)
+	
+def readPrivateKeyFromCertFile(path):
+	data = readKeyFromCertFile(path)
+	fields = readASN1Data(data)
+	
+	data = fields[2][1]
+	
+	fields = readASN1Data(data)
+	
+	# [2] modulus
+	
+	#for field in fields:
+		#print('type:%s data-len:%s' % (field[0], len(field[1]) * 8))
+		#print('data:%s' % base64.b64encode(field[1])[0:10])
+	
+	pubk = fields[1][1]
+	exp = fields[2][1]
+	prik = fields[3][1]
+
+	pubk = fromi256(toi256r(pubk))
+	prik = fromi256(toi256r(prik))
+	exp = fromi256(toi256r(exp))	
+	
+	pubkey = (exp, pubk)
+	prikey = (prik, pubk)
+	
+	#msg = b'hello world'
+	#coded = crypt(msg, pubkey)
+	#plain = decrypt(coded, prikey)
+	#if msg == plain:
+	#	#print(msg)
+	#	#print(plain)
+	#	print('@@@@@@ FOUND IT')
+	#	print()
+	#exit()
+	return (pubkey, prikey)
 	
 def readKeyFile(path):
 	fd = open(path, 'r')
@@ -103,7 +166,13 @@ def readSSHPublicKey(path):
 	
 def readSSHPrivateKey(path):
 	data = readKeyFile(path)
+	return __readSSHPrivateKey(data)
 	
+def __readSSHPrivateKey(data):
+	fields = readASN1Data(data)
+	return (fields[3][1], fields[1][1])
+	
+def readASN1Data(data):
 	fields = []
 	
 	data = data[4:]
@@ -113,11 +182,29 @@ def readSSHPrivateKey(path):
 		type = data[0]
 		data = data[1:]
 		
+		#print('type:%s' % type)
+		
+		if type & 0x40 == 0x40:
+			raise Exception('ASN.1 Entry Used Indefinite Length')
+		
+		# read the first length
 		sz = data[0]
 		data = data[1:]
 		
-		if sz == 0x82:
+		#print('sz:%s' % sz)
+		
+		# if bit 7 is set then we use more bytes for the length
+		if sz & 0x80 == 0x80:
+			# get the length of the length field
+			c = sz & 0x7F
+			# make sure we are not in over our head and
+			# if so then let someone know that we are
+			if c != 2:
+				raise Exception('ASN.1 Entry Used Length File Not 2 Bytes')
+			# read the length of the data
 			sz = data[0] << 8 | data[1]
+			#print('   2sz:%s' % sz)
+			# remove the length
 			data = data[2:]
 		
 		# create field
@@ -129,8 +216,8 @@ def readSSHPrivateKey(path):
 		fields.append(field) 
 		fields[ndx] = field
 		ndx = ndx + 1
-		
-	return (fields[3][1], fields[1][1])
+	
+	return fields
 	
 '''
 msg = 'hello'
