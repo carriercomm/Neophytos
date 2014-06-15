6bc70e639cc40a7791b1e790177cc9dd3e159d8e
kmcguire <kmcg3413@gmail.com>
2014-06-15 17:18:02 -0500

    working on clientgui

--- clientgui.py
+++ clientgui.py
@@ -7,27 +7,8 @@ import backup
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 import io
-		
-class StatusWindow(QtGui.QMainWindow):
-	def __init__(self):
-		QtGui.QMainWindow.__init__(self)
-		
-		self.resize(600, 340)
-		self.move(400, 20)
-		self.setWindowTitle('Neophytos Backup Status')
-		self.show()
-		
-		self.setStyleSheet('')
-		
-		icon = QtGui.QIcon('./media/book.ico')
-		self.setWindowIcon(icon)
-		
-		accounts = self.GetAccounts()
-		for account in accounts:
-			targets = self.GetTargets(account)
-			for target in targets:
-				print(account, target)
-			
+
+class ClientInterface:
 	'''
 		The following functions form an textual stream interface between the
 		backup command line utility. I got down and thought hard about making
@@ -42,8 +23,8 @@ class StatusWindow(QtGui.QMainWindow):
 		a backup_comm.py file which including doing everything needed to return
 		the data. Basically, backup_comm.py would implement the interface below.
 	'''
-	def GetTargets(self, account):
-		out = self.ConsoleCommand([account, 'list'])
+	def GetTargets(account):
+		out = ClientInterface.ConsoleCommand([account, 'list'])
 		lines = out.split('\n')
 		targets = {}
 		for line in lines:
@@ -65,8 +46,8 @@ class StatusWindow(QtGui.QMainWindow):
 					filters.append(filter)
 		return targets
 	
-	def GetAccounts(self):
-		out = self.ConsoleCommand(['list'])
+	def GetAccounts():
+		out = ClientInterface.ConsoleCommand(['list'])
 		lines = out.split('\n')
 		accounts = []
 		for line in lines:
@@ -74,7 +55,7 @@ class StatusWindow(QtGui.QMainWindow):
 				accounts.append(line.strip())
 		return accounts
 		
-	def ConsoleCommand(self, cmd):
+	def ConsoleCommand(cmd):
 		ca = backup.ConsoleApplication()
 		oldstdout = sys.stdout
 		buf = io.StringIO()
@@ -83,8 +64,79 @@ class StatusWindow(QtGui.QMainWindow):
 		sys.stdout = oldstdout
 		buf.seek(0)
 		return buf.read()
+
 		
+class QAccountsAndTargetSystem(QtGui.QFrame):
+	def __init__(self, parent):
+		QtGui.QFrame.__init__(self, parent)
+		self.Create()
 	
+	def Create(self):
+		accounts = ClientInterface.GetAccounts()
+
+		table = QtGui.QTableWidget(self)
+		table.resize(self.width(), self.height())
+		self.table = table
+		
+		table.setColumnCount(5)
+		table.setHorizontalHeaderLabels(('Account', 'Target', 'Path', 'Enabled', 'Status'))
+		
+		for account in accounts:
+			targets = ClientInterface.GetTargets(account)
+			for target in targets:
+				acctarname = '%s.%s' % (account, target)
+				
+				table.insertRow(0)
+				
+				item = QtGui.QTableWidgetItem()
+				item.setText(account)
+				table.setItem(0, 0, item)
+				item = QtGui.QTableWidgetItem()
+				item.setText(target)
+				table.setItem(0, 1, item)
+				item = QtGui.QTableWidgetItem()
+				item.setText(targets[target]['disk-path'])
+				table.setItem(0, 2, item)
+				# create new frame to hold information
+				#self.apanel[acctarname] = QtGui.QFrame(self)
+				# create account name and target name fields
+				#self.apanel[acctarname].setLayout(lo)
+				#lo.setContentsMargin(0, 0, 0, 0)
+				
+				print(acctarname)
+		
+		#self.setLayout(vlo)
+		self.show()
+	
+	def resize(self, w, h):
+		super().resize(w, h)
+		self.table.resize(w, h)
+				
+				
+class QStatusWindow(QtGui.QMainWindow):
+	def resizeEvent(self, event):
+		self.fAccountsAndTargets.resize(self.width(), self.height())
+
+	def resize(self, w, h):
+		super().resize(w, h)
+		
+	def __init__(self):
+		QtGui.QMainWindow.__init__(self)
+		
+		self.setStyleSheet('')
+		
+		icon = QtGui.QIcon('./media/book.ico')
+		self.setWindowIcon(icon)
+	
+		self.fAccountsAndTargets = QAccountsAndTargetSystem(self)
+		self.fAccountsAndTargets.resize(self.width(), self.height())
+
+		self.resize(400, 340)
+		self.move(400, 20)
+		self.setWindowTitle('Neophytos Backup Status')
+		self.show()
+
+		
 def main():
 	app = QtGui.QApplication(sys.argv)
 	# Cleanlooks
@@ -94,7 +146,7 @@ def main():
 	style = QtGui.QStyleFactory.create('Plastique')
 	app.setStyle(style)
 
-	w = StatusWindow()
+	w = QStatusWindow()
 				
 	sys.exit(app.exec_())
 	
